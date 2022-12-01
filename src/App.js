23ca5df0f21369071702a4a3d50debefc965ce22
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-12-01 11:41:45 +0530

    understanding props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import GreetPersons from './components/GreetPersons';
 
 function App() {
   return (
     <div className="App">
       <Greet/>
       <Welcome/>
+      <GreetPersons name="Raghu"></GreetPersons>
+      <GreetPersons name="Srinu"></GreetPersons>
     </div>
   );
 }
---
+++ src/components/GreetPersons.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GreetPersons(props) {
+  return (
+    <>
+      <h4>Hello {props.name}</h4>
+    </>
+  );
+}
+
+export default GreetPersons;
