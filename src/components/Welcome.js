ef756504e69fb818bd860fede46d1f7a2aa16b44
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-12-01 11:08:22 +0530

    created a class component

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <>
+        <h3>This is a class component</h3>
+      </>
+    );
+  }
+}
