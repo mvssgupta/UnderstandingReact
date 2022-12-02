5df12547baa1105975572f65c00b6833f96ac3fe
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-12-02 13:49:07 +0530

    added comments

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Child(props) {
   return (
     <>
+        {/* to pass parameters from childComp to parentComp we have to use arrow functions */}
         <button onClick={() => props.WishHandler("Gupta")}>Call Parent</button>
     </>
   )
