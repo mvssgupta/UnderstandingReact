925e47943ea8cad62eabcbdeac997c4d5888b3d1
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-12-03 12:09:32 +0530

    added keys to items in map

--- src/components/ObjectList.js
+++ src/components/ObjectList.js
@@ -6,8 +6,11 @@ function ObjectList() {
     { id: 2, name: "Srinu", cls: "Inter" },
     { id: 3, name: "Raghu", cls: "B.tech" },
   ];
+
+//   every item in a map should have a unique value called key
+// here the key should be unique , since here the student id is unique we can use it as key
   const studentsList = students.map((object) => (
-    <h2>
+    <h2 key = {object.id}>
       Hello this is {object.name} , studying {object.cls}
     </h2>
   ));
