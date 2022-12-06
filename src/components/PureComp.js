897a23a7f5a0132a980e9ad468dfa2ddbb9ba5b3
M.V.S.S.Gupta <mvssguptajagadesh@gmail.com>
2022-12-06 23:56:23 +0530

    Learnt about pure components in react

--- src/components/PureComp.js
+++ src/components/PureComp.js
@@ -4,9 +4,26 @@ export class PureComp extends PureComponent {
   render() {
     console.log("Render method of Pure component")
     return (
-      <div>PureComponent {this.props.name}</div>
+      <>
+      PureComponent {this.props.name}
+      <h3>Summary of Pure component</h3>
+      <ul>
+       <li>We can create a Pure component by extending the PureComponent class.</li>
+        
+       <li>A PureComponent implements the shouldComponentUpdate lifecycle method
+        by performing shallow comparison on the props and the state of component</li>
+
+       <li>If there is no differnce , then the component is not rendered - then it boosts the performance.</li>
+        
+       <li>It is a good idea to ensure that all the children components are also 
+        pure to avoid unexpected behaviour.</li>
+        
+       <li>Never mutate the state. Always return a new object that reflects the new state.</li>
+        
+      </ul>
+      </>
     )
-  }
+  } 
 }
 
 export default PureComp
